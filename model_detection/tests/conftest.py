8ef8f439905bdb9a7401bc00a3be28b18b2072f8
saaaaaaaaaaaaash <just4n4cc@yandex.ru>
2024-05-31 12:29:36 +0300

    precommit fixes

--- model_detection/tests/conftest.py
+++ model_detection/tests/conftest.py
@@ -1,9 +1,10 @@
-from model_detection import ObjectDetection
 import pytest
+
+from model_detection import ObjectDetection
 
 model = ObjectDetection()
 
 
-@pytest.fixture(scope='session', name='model')
+@pytest.fixture(scope="session", name="model")
 def model_fixture():
     return model
